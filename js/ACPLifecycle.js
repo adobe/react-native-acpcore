fd4352cab27bc38da705ce9da8c5124295fda53d
nporter <nporter@adobe.com>
2019-02-06 14:01:59 -0700

    Lifecycle fix

--- js/ACPLifecycle.js
+++ js/ACPLifecycle.js
@@ -15,26 +15,12 @@
 * @flow
 * @format
 */
-
 'use strict';
 
 const RCTACPLifecycle = require('react-native').NativeModules.ACPLifecycle;
 
 module.exports = {
-
-  /**
-   * Returns the version of the ACPLifecycle extension
-   * @param  {string} Promise [description]
-   */
-  extensionVersion(): Promise<string> {
-    return Promise.resolve("1.0.0");
-  },
-
-  /**
-   * Registers the ACPLifecycle extension with ACPCore
-   */
   registerExtension() {
     RCTACPLifecycle.registerExtension();
   },
-
 };
