4280e46c634967c6b4dfbab58e847ba2fd127fb6
nporter <nporter@adobe.com>
2019-04-01 16:17:19 -0600

    Use @adobe scope

--- sample/ACPCoreSample/android/app/src/main/java/com/acpcoresample/MainApplication.java
+++ sample/ACPCoreSample/android/app/src/main/java/com/acpcoresample/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.adobe.marketing.mobile.reactnative.RCTACPCorePackage;
+import com.adobe.marketing.mobile.reactnative.RCTACPCorePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTACPCorePackage(),
             new RCTACPCorePackage()
       );
     }
