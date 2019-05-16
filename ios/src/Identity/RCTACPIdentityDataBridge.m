3f9ca0d17dab7ff91bf0e28e8a1d94c7e3b6d556
nporter <nporter@adobe.com>
2019-05-16 14:11:42 -0600

    Update header comments

--- ios/src/Identity/RCTACPIdentityDataBridge.m
+++ ios/src/Identity/RCTACPIdentityDataBridge.m
@@ -1,19 +1,14 @@
-/*************************************************************************
- * ADOBE CONFIDENTIAL
- * ___________________
- *
- * Copyright 2019 Adobe
- * All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains
- * the property of Adobe and its suppliers, if any. The intellectual
- * and technical concepts contained herein are proprietary to Adobe
- * and its suppliers and are protected by all applicable intellectual
- * property laws, including trade secret and copyright laws.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe.
- **************************************************************************/
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 
 #import "RCTACPIdentityDataBridge.h"
 
@@ -36,7 +31,7 @@ static NSString* const VISITOR_ID_AUTH_STATE_KEY = @"authenticationState";
     visitorIdDict[VISITOR_ID_ID_TYPE_KEY] = visitorId.idType;
     visitorIdDict[VISITOR_ID_ID_KEY] = visitorId.identifier;
     visitorIdDict[VISITOR_ID_AUTH_STATE_KEY] = [RCTACPIdentityDataBridge stringFromAuthState:visitorId.authenticationState];
-    
+
     return visitorIdDict;
 }
 
@@ -57,7 +52,7 @@ static NSString* const VISITOR_ID_AUTH_STATE_KEY = @"authenticationState";
     } else if ([authStateString isEqualToString:ACP_VISITOR_AUTH_STATE_LOGGED_OUT]) {
         return ACPMobileVisitorAuthenticationStateLoggedOut;
     }
-    
+
     return ACPMobileVisitorAuthenticationStateUnknown;
 }
 
