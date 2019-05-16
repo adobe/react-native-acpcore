dff890ea9c71a1a530903a6f3decf8e3465ced1e
nporter <nporter@adobe.com>
2019-05-16 14:11:42 -0600

    Update header comments

--- ios/src/Data-Bridging/ACPExtensionEvent+ReactNative.m
+++ ios/src/Data-Bridging/ACPExtensionEvent+ReactNative.m
@@ -1,19 +1,14 @@
-/*************************************************************************
- * ADOBE CONFIDENTIAL
- * ___________________
- *
- * Copyright 2019 Adobe
- * All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains
- * the property of Adobe and its suppliers, if any. The intellectual
- * and technical concepts contained herein are proprietary to Adobe
- * and its suppliers and are protected by all applicable intellectual
- * property laws, including trade secret and copyright laws.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe.
- **************************************************************************/
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 
 #import "ACPExtensionEvent+ReactNative.h"
 
@@ -28,11 +23,11 @@ static NSString* const EVENT_DATA_KEY = @"eventData";
     NSString *name = [dict objectForKey:EVENT_NAME_KEY];
     NSString *type = [dict objectForKey:EVENT_TYPE_KEY];
     NSString *source = [dict objectForKey:EVENT_SOURCE_KEY];
-    
+
     if (name && type && source && [[dict objectForKey:EVENT_DATA_KEY] isKindOfClass:[NSDictionary class]]) {
         return [ACPExtensionEvent extensionEventWithName:name type:type source:source data:[dict objectForKey:EVENT_DATA_KEY] error:nil];
     }
-    
+
     return nil;
 }
 
@@ -42,7 +37,7 @@ static NSString* const EVENT_DATA_KEY = @"eventData";
     eventDict[EVENT_TYPE_KEY] = event.eventType;
     eventDict[EVENT_SOURCE_KEY] = event.eventSource;
     eventDict[EVENT_DATA_KEY] = event.eventData;
-    
+
     return eventDict;
 }
 
